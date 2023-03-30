ebd4ef4cda70aff6124f0150c4443e253e2e355d
Apollo-XIV <asdfafdafsd@gmail.com>
2023-03-30 19:35:48 +0100

    adsf

--- Git Demo/Git Demo/Program.vb
+++ Git Demo/Git Demo/Program.vb
@@ -1,8 +1,6 @@
 Imports System
 
 Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        asdf        Console.WteLine("teastestes")
+    Sub Mansole.WteLine("teastestes")
     End Sub
 End Module
